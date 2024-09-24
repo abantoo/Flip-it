fff82bb0151d9318bd81f8ff737fd81162579e1b
abantoo <abantonaimur@gmail.com>
2024-09-24 23:44:33 +0600

    fixes indent for components

--- src/components/FeaturedCard.vue
+++ src/components/FeaturedCard.vue
@@ -1,19 +1,18 @@
 <template>
-    <div class="border h-42 border-amber-300 flex justify-center rounded-md p-4">
-        <img class="w-32" :src="props.image" alt="">   
-    </div>
+  <div class="border h-42 border-amber-300 flex justify-center rounded-md p-4">
+    <img class="w-32" :src="props.image" alt="" />
+  </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps({
-    title: {
-        type: String,
-        required: true,
-    },
-    image: {
-        type: String,
-        required: true,
-    },
-    
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
 </script>
