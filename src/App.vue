2c2cb287e76629d3aa1126a91320c7a6ab982bec
abantoo <abantonaimur@gmail.com>
2025-03-02 23:55:41 +0600

    fixes header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center min-h-screen font-montserrat" data-theme="light">
-    <NavBar />
-    <div class="container pt-32">
+    <NavBar class="sticky top-0"/>
+    <div class="container pt-14">
       <RouterView/>
     </div>
   </div>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,32 +1,34 @@
 <template>
-  <nav class="flex items-center justify-between h-18 py-4 px-8 bg-black shadow-md shadow-slate-400 w-full">
-    <RouterLink to="/">
-      <img class="w-12" src="/flip_it.png" alt="flip-it-bd" />
-    </RouterLink>
-    <div class="text-xl flex items-center justify-between gap-8 text-white">
-      <RouterLink
-        to="/"
-        class="hover:text-amber-300 border border-transparent hover:border-amber-300 hover:rounded p-2 hover:transition-all"
-        :class="{
-          'text-amber-300': router.currentRoute.value.name == 'home',
-        }"
-      >
-        Home
+  <nav class="flex items-center justify-center bg-black shadow-md shadow-slate-400 w-full">
+    <div class="container flex items-center justify-between bg-black shadow-md w-full">
+      <RouterLink to="/">
+        <img class="h-16" src="/flip_it.png" alt="flip-it-bd" />
       </RouterLink>
-      <RouterLink
-        to="/about"
-        class="hover:text-amber-300 border border-transparent hover:border-amber-300 hover:rounded p-2 hover:transition-all"
-        :class="{
-          'text-amber-300': router.currentRoute.value.name == 'about',
-        }"
-      >
-        About
-      </RouterLink>
-      <div>
-        <PhShoppingCart :size="32" />
-      </div>
-      <div>
-        <PhUserCircle :size="32" />
+      <div class="text-xl flex items-center justify-between gap-8 text-white">
+        <RouterLink
+          to="/"
+          class="hover:text-amber-300 border border-transparent hover:border-amber-300 hover:rounded p-2 hover:transition-all"
+          :class="{
+            'text-amber-300': router.currentRoute.value.name == 'home',
+          }"
+        >
+          Home
+        </RouterLink>
+        <RouterLink
+          to="/about"
+          class="hover:text-amber-300 border border-transparent hover:border-amber-300 hover:rounded p-2 hover:transition-all"
+          :class="{
+            'text-amber-300': router.currentRoute.value.name == 'about',
+          }"
+        >
+          About
+        </RouterLink>
+        <div>
+          <PhShoppingCart :size="32" />
+        </div>
+        <div>
+          <PhUserCircle :size="32" />
+        </div>
       </div>
     </div>
   </nav>
