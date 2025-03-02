0ac9d1f8e7240f565e38bb1ef2d1b390f676c61a
abantoo <abantonaimur@gmail.com>
2025-03-03 00:31:10 +0600

    adds some content in footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,7 +1,60 @@
 <template>
-    <div class="bg-slate-700 w-full flex items-center justify-center">
-        <div class="container flex items-center justify-between">
-            This is the footer.
+    <div class="bg-slate-900 w-full flex flex-col items-center justify-center">
+        <div class="container flex items-center justify-between text-white">
+            <!-- Subscribe -->
+            <div class="text-white">
+                <h1>Subscribe To Our Newsletter</h1>
+                <h2>Want to stay updated about current listings and learn about current market?</h2>
+                <h2>Subscribe to our NewsLetter today</h2>
+
+                <button class="btn btn-warning">Subscribe</button>
+            </div>
+
+            <!-- About Us -->
+            <div>
+                <h1>About Us</h1>
+                <a href="">Our Team</a>
+                <a href="">Career</a>
+                <a href="">Online Delivery</a>
+                <a href="">Blog</a>
+            </div>
+
+            <!-- Contact Us -->
+            <div>
+                <h1>Stay Connected</h1>
+                <div>
+                    <h1>Head Office</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Eum facilis voluptatem omnis vitae accusantium ex nihil ab fugiat officia quisquam 
+                        ea similique optio dolor sed dicta temporibus perferendis, nulla cupiditate?
+                    </p>
+                </div>
+                <div>
+                    <h1>Corporate Office</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Eum facilis voluptatem omnis vitae accusantium ex nihil ab fugiat officia quisquam 
+                        ea similique optio dolor sed dicta temporibus perferendis, nulla cupiditate?
+                    </p>
+                </div>
+
+                <div>
+                    <h1>Distribution Center</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Eum facilis voluptatem omnis vitae accusantium ex nihil ab fugiat officia quisquam 
+                        ea similique optio dolor sed dicta temporibus perferendis, nulla cupiditate?
+                    </p>
+                </div>
+
+                <div>
+                    email: random-email@email.com
+                </div>
+            </div>
         </div>
+        <p class="text-gray-300 text-sm">
+            ©{{ new Date().getFullYear() }} Flip-IT bd Ltd | All rights reserved.
+        </p>
     </div>
 </template>
