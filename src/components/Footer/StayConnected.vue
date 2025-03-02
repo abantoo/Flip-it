296c2339f18da8d9aee9d63a5dc7a935c7a77457
abantoo <abantonaimur@gmail.com>
2025-03-03 00:39:07 +0600

    adds stay connected contents to component

---
+++ src/components/Footer/StayConnected.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Stay Connected</h1>
+  <div>
+    <h1>Head Office</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum facilis
+      voluptatem omnis vitae accusantium ex nihil ab fugiat officia quisquam ea
+      similique optio dolor sed dicta temporibus perferendis, nulla cupiditate?
+    </p>
+  </div>
+  <div>
+    <h1>Corporate Office</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum facilis
+      voluptatem omnis vitae accusantium ex nihil ab fugiat officia quisquam ea
+      similique optio dolor sed dicta temporibus perferendis, nulla cupiditate?
+    </p>
+  </div>
+
+  <div>
+    <h1>Distribution Center</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum facilis
+      voluptatem omnis vitae accusantium ex nihil ab fugiat officia quisquam ea
+      similique optio dolor sed dicta temporibus perferendis, nulla cupiditate?
+    </p>
+  </div>
+
+  <div>email: random-email@email.com</div>
+</template>
