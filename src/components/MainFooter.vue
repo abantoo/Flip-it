a7b47a4d0d7014d7425656e6cc204a9d27bc4bfe
abantoo <abantonaimur@gmail.com>
2025-03-03 00:07:51 +0600

    adds some content in footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
-        This is the footer.
+    <div class="bg-slate-700 w-full flex items-center justify-center">
+        <div class="container flex items-center justify-between">
+            This is the footer.
+        </div>
     </div>
 </template>
