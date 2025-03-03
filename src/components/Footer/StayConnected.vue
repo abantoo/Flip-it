0a7e6746828f924e02f675ab6a6435b5167ef8f3
abantoo <abantonaimur@gmail.com>
2025-03-04 01:14:14 +0600

    updates stay connected section

--- src/components/Footer/StayConnected.vue
+++ src/components/Footer/StayConnected.vue
@@ -1,5 +1,39 @@
 <template>
   <h1 class="text-2xl font-bold">Stay Connected</h1>
+  <div 
+    v-for="office in offices" 
+    :key="office.name"
+    class="py-2"
+  >
+    <h2 class="text-lg font-medium">{{ office.name }}</h2>
+    <div class="flex items-center gap-1">
+      <component :is="office.logo" />
+      <p class="text-sm">{{ office.location }}</p>
+    </div>
+  </div>
+
+  <div class="flex items-center gap-2 mt-2"> 
+    <PhEnvelopeSimple :size="24" /> 
+    {{ email }}
+  </div>
+
+  <button 
+    type="button" 
+    v-for="social in socials"
+    class="py-4"
+  >
+    <component 
+      :size="32"
+      :is="social.logo"
+      class="transition-colors rounded border 
+      border-transparent hover:border-white hover:bg-slate-200"
+      :class="{
+        'hover:text-black' : social.name === 'x',
+        'hover:text-[#8a3ab9] mx-2': social.name === 'instagram',
+        'hover:text-[#1877F2]': social.name === 'facebook',
+      }"
+    />
+  </button>
 </template>
 
 <script lang="ts" setup>
@@ -8,7 +42,7 @@ import {
   PhFacebookLogo,
   PhXLogo,
   PhMapPin,
-  PhMailbox,
+  PhEnvelopeSimple,
 } from '@phosphor-icons/vue';
 
 const offices = [
@@ -22,5 +56,22 @@ const offices = [
     location: 'Nowhere, Dhaka, Bangladesh',
     logo: PhMapPin,
   },
-]
+];
+
+const socials = [
+  {
+    name: 'facebook',
+    logo: PhFacebookLogo,
+  },
+  {
+    name: 'instagram',
+    logo: PhInstagramLogo,
+  },
+  {
+    name: 'x',
+    logo: PhXLogo,
+  }
+];
+
+const email = "random-email@email.com";
 </script>
