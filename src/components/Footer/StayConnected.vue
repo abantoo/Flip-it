7668d6a1ee5d9730da61ac45b59631c7d57f6132
abantoo <abantonaimur@gmail.com>
2025-03-04 00:58:49 +0600

    removes placeholder data and adds script

--- src/components/Footer/StayConnected.vue
+++ src/components/Footer/StayConnected.vue
@@ -1,18 +1,5 @@
 <template>
   <h1 class="text-2xl font-bold">Stay Connected</h1>
-  <div 
-    v-for="office in offices" 
-    :key="office.name"
-    class="py-2"
-  >
-    <h2 class="text-lg font-medium">{{ office.name }}</h2>
-    <div class="flex items-center gap-1">
-      <component :is="office.logo" />
-      <p class="text-sm">{{ office.location }}</p>
-    </div>
-  </div>
-
-  <div><span class="font-semibold">Email: </span> random-email@email.com</div>
 </template>
 
 <script lang="ts" setup>
@@ -21,6 +8,7 @@ import {
   PhFacebookLogo,
   PhXLogo,
   PhMapPin,
+  PhMailbox,
 } from '@phosphor-icons/vue';
 
 const offices = [
