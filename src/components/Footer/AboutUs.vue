44f0f27c6cf8dd079f2523faebcfaa84ed636af5
abantoo <abantonaimur@gmail.com>
2025-03-05 00:55:00 +0600

    adds a separate section for about us

---
+++ src/components/Footer/AboutUs.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold">About Us</h1>
+    <ul class="flex flex-col gap-2 py-2">
+      <li v-for="option in options">
+        <a href="">{{ option.name }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const options = [
+  {
+    name: 'Our Team'
+  },
+  {
+    name: 'Career'
+  },
+  {
+    name: 'Online Delivery'
+  },
+  {
+    name: 'Blog'
+  },
+]
+</script>
