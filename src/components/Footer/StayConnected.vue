8bb577e83f3d27f5189cf1bb8a3f18182cd79aaa
abantoo <abantonaimur@gmail.com>
2025-03-04 23:17:59 +0600

    adds a wrapper and comments

--- src/components/Footer/StayConnected.vue
+++ src/components/Footer/StayConnected.vue
@@ -1,39 +1,46 @@
 <template>
-  <h1 class="text-2xl font-bold">Stay Connected</h1>
-  <div 
-    v-for="office in offices" 
-    :key="office.name"
-    class="py-2"
-  >
-    <h2 class="text-lg font-medium">{{ office.name }}</h2>
-    <div class="flex items-center gap-1">
-      <component :is="office.logo" />
-      <p class="text-sm">{{ office.location }}</p>
+  <div>
+    <!-- Title -->
+    <h1 class="text-2xl font-bold">Stay Connected</h1>
+
+    <!-- Locations -->
+    <div 
+      v-for="office in offices" 
+      :key="office.name"
+      class="py-2"
+    >
+      <h2 class="text-lg font-medium">{{ office.name }}</h2>
+      <div class="flex items-center gap-1">
+        <component :is="office.logo" />
+        <p class="text-sm">{{ office.location }}</p>
+      </div>
     </div>
-  </div>
 
-  <div class="flex items-center gap-2 mt-2"> 
-    <PhEnvelopeSimple :size="24" /> 
-    {{ email }}
-  </div>
+    <!-- Email -->
+    <div class="flex items-center gap-2 mt-2"> 
+      <PhEnvelopeSimple :size="24" /> 
+      {{ email }}
+    </div>
 
-  <button 
-    type="button" 
-    v-for="social in socials"
-    class="py-4"
-  >
-    <component 
-      :size="32"
-      :is="social.logo"
-      class="transition-colors rounded border 
-      border-transparent hover:border-white hover:bg-slate-200"
-      :class="{
-        'hover:text-black' : social.name === 'x',
-        'hover:text-[#8a3ab9] mx-2': social.name === 'instagram',
-        'hover:text-[#1877F2]': social.name === 'facebook',
-      }"
-    />
-  </button>
+    <!-- Socials -->
+    <button 
+      type="button" 
+      v-for="social in socials"
+      class="py-4"
+    >
+      <component 
+        :size="32"
+        :is="social.logo"
+        class="transition-colors rounded border 
+        border-transparent hover:border-white hover:bg-slate-200"
+        :class="{
+          'hover:text-black' : social.name === 'x',
+          'hover:text-[#8a3ab9] mx-2': social.name === 'instagram',
+          'hover:text-[#1877F2]': social.name === 'facebook',
+        }"
+      />
+    </button>
+  </div>
 </template>
 
 <script lang="ts" setup>
