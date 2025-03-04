8fc0010506c20074a9823841bd5775400283d345
abantoo <abantonaimur@gmail.com>
2025-03-05 00:55:39 +0600

    adds a separate component for news letter

---
+++ src/components/Footer/NewsLetter.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-white">
+    <h1 class="text-2xl font-bold mb-2">Subscribe To Our Newsletter</h1>
+    <h2>Want to stay updated about current listings and learn about current market?</h2>
+    <h2>Subscribe to our NewsLetter today</h2>
+
+    <button class="btn btn-warning mt-2">Subscribe</button>
+  </div>
+</template>
