b1becd1c8dd12e0fdc4202e8139d8ad0f756d99f
abantoo <abantonaimur@gmail.com>
2025-03-06 02:28:02 +0600

    redesigns itemcard

--- src/components/ItemsCard.vue
+++ src/components/ItemsCard.vue
@@ -1,20 +1,34 @@
 <template>
-  <div class="border border-amber-300 flex flex-col gap-4 rounded-md p-4">
+  <div class="flex flex-col gap-4 rounded-md p-4 sm:max-w-[296px] shadow-sm shadow-slate-300 border border-slate-300/20 bg-slate-100">
     <h1>{{ props.title }}</h1>
-    <div class="w-full flex justify-between gap-2">
-      <div class="">
-        <img class="h-48 lg:w-52 w-36 md:w-48" :src="props.image" alt="" />
+    <div class="w-full flex sm:flex-col justify-between gap-2">
+      <div class="bg-pattern">
+        <img 
+          class="h-36 min-w-36 sm:h-48 lg:w-full object-contain w-36 md:w-48 rounded border border-slate-300/60 shadow-md shadow-slate-300" 
+          :src="props.image" alt="" />
       </div>
-      <div class="flex flex-col">
-        <h1
-          class="text-right border border-emerald-500 bg-emerald-500 text-black rounded px-4"
-        >
-          Price: {{ props.price }}
-        </h1>
-        <h1 class="text-right">Condition: {{ props.condition }}</h1>
+      <div class="px-2 flex flex-col">
+        <div class="flex justify-between items-center">
+          <h1
+            class="text-black rounded"
+          >
+            {{ props.price }}
+          </h1>
+          <h1
+            class="px-2 py-1 w-fit rounded shadow-sm shadow-slate-300 text-sm"
+            :class="{
+              'bg-yellow-400': condition === 'Used',
+              'bg-green-400': condition === 'New', 
+            }"
+          >
+            {{ props.condition }}
+          </h1>
+        </div>
+        <h1 class="text-start text-sm font-light">{{ props.description }}</h1>
       </div>
+      
     </div>
-    <h1 class="text-center">{{ props.description }}</h1>
+    
   </div>
 </template>
 
@@ -46,3 +60,10 @@ const props = defineProps({
   },
 });
 </script>
+
+<style scoped>
+.bg-pattern {
+  background-color: #cbd5e1;
+  background-image: url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-6 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%230f172a' fill-opacity='0.26' fill-rule='evenodd'/%3E%3C/svg%3E");
+}
+</style>
