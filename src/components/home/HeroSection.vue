ac5b5c5b3fcf42e09b35663e0974eebd8aeca626
abantoo <abantonaimur@gmail.com>
2025-04-18 12:45:00 +0600

    style: reduce top padding in HeroSection for tighter layout

--- src/components/home/HeroSection.vue
+++ src/components/home/HeroSection.vue
@@ -8,7 +8,7 @@
       <div class="absolute top-1/2 -right-24 w-80 h-80 bg-blue-500/10 blur-[100px] rounded-full"></div>
     </div>
 
-    <div class="relative z-10 w-full flex flex-col items-center pt-16 pb-40 lg:pt-20 lg:pb-0 lg:h-full lg:justify-center px-6 text-center">
+    <div class="relative z-10 w-full flex flex-col items-center pt-10 pb-40 lg:pt-14 lg:pb-0 lg:h-full lg:justify-center px-6 text-center">
       <h1 class="text-5xl md:text-7xl font-black text-white tracking-tight mb-6 max-w-4xl">
         Level Up Your Rig with <span class="bg-gradient-to-r from-amber-400 to-amber-200 bg-clip-text text-transparent">Flip-it</span>
       </h1>
