2b8c6683cd3147dea4853d18112a1b08a2a1299a
abantoo <abantonaimur@gmail.com>
2025-05-29 11:00:00 +0600

    config: add office and support fields to AppConfig interface

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,9 @@ export interface AppConfig {
   appName: string;
   theme: string;
   currency: string;
+  headOffice: string;
+  corporateOffice: string;
+  supportEmail: string;
 }
 
 export const appConfig: AppConfig = config;
