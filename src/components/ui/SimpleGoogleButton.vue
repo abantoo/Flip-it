8a04f8e9906d1fcff8b4281580aa4744597de525
abantoo <abantonaimur@gmail.com>
2025-07-25 09:20:00 +0600

    feat: add SimpleGoogleButton component

---
+++ src/components/ui/SimpleGoogleButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    type="button"
+    @click="$emit('click')"
+    class="w-full flex items-center justify-center gap-3 px-6 py-3 bg-white border-2 border-slate-200 hover:border-slate-900 transition-all duration-200 text-slate-700 font-bold"
+  >
+    <GoogleIcon />
+    <span>Sign in with Google</span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import GoogleIcon from './icons/GoogleIcon.vue';
+
+defineEmits(['click']);
+</script>
