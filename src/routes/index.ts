6ab03297ada84c38b48d33ec8c02d8df4599f20e
abantoo <abantonaimur@gmail.com>
2025-08-01 14:45:00 +0600

    refactor: tidy up route configurations

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,46 +1,41 @@
-import {
-    createRouter,
-    createWebHistory,
-    RouteRecordRaw,
-  } from 'vue-router';
-  
-  const routes: Array<RouteRecordRaw> = [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import("../views/HomeView.vue"),
-      
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import("../views/AboutUs.vue")
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import("../views/auth/LoginView.vue")
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import("../views/auth/RegisterView.vue")
-    },
-    {
-      path: '/forgot-password',
-      name: 'forgot-password',
-      component: () => import("../views/auth/ForgotPasswordView.vue")
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'Error',
-      component: () => import('../views/ErrorPage.vue'),
-    },
-  ]
-  
-  const router = createRouter({
-      history: createWebHistory(),
-      routes
-  }) 
-  
-  export default router;
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue'),
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutUs.vue'),
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/auth/LoginView.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/auth/RegisterView.vue'),
+  },
+  {
+    path: '/forgot-password',
+    name: 'forgot-password',
+    component: () => import('../views/auth/ForgotPasswordView.vue'),
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'Error',
+    component: () => import('../views/ErrorPage.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
