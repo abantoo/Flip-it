e3643b4f486ebcf05a45ab04201fe16c95d1f44f
abantoo <abantonaimur@gmail.com>
2025-08-01 11:30:00 +0600

    update: format main entry point

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
-import './index.css'
-import './styles/main.scss'
-import router from "./routes";
-import App from './App.vue'
+import { createApp } from 'vue';
+import './index.css';
+import './styles/main.scss';
+import router from './routes';
+import App from './App.vue';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router).mount('#app')
+app.use(router).mount('#app');
